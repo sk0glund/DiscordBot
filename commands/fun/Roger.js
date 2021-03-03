7f53a37e8145334927b4a037bce4182b93cfa7cb
sk0glund <adam.tiger.skoglund@gmail.com>
2021-03-03 20:10:56 +0100

    Added the roger feature and changed template

---
+++ commands/fun/Roger.js
@@ -0,0 +1,10 @@
+module.exports = {
+	name: 'roger',
+	description: 'Taggs user @Roger0103',
+    args: false,
+    guildOnly: true,
+    cooldown: 0,
+    execute(message, args) {
+        message.channel.send(`Hej <@342027217313726476>`);
+	},
+};
